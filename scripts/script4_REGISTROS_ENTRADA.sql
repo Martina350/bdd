a6aa5f8c168dc1f7d70c24bf2475f9337eb8ddba
Martina350 <martinacamilaleon@gmail.com>
2024-09-17 20:53:59 -0500

    SE CREAN Y EJECUTAN 10 INSERTS EN LA TABLA REGISTROS ENTRADA

--- scripts/script4_REGISTROS_ENTRADA.sql
+++ scripts/script4_REGISTROS_ENTRADA.sql
@@ -6,3 +6,35 @@ create table registros_entrada(
 	hora time not null,
 	constraint registros_entrada_pk primary key (codigo_registro)
 )
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('52863','1748963525','30/10/2001','14:42')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('89674','1710113040','05/08/2004','16:05')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('31325','0985742163','11/01/2008','20:14')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('85964','1758241698','29/07/2010','09:31')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('47142','1712132524','13/04/2012','15:59')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('13245','0987456123','15/01/2013','10:00')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('27456','1721222354','21/03/2016','23:59')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('36945','1758565425','04/06/2017','13:12')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('24875','1779964112','17/11/2019','22:40')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values('64789','0960035847','21/10/2024','20:51')
+
+select*from registros_entrada
