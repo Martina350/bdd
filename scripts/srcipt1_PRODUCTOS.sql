10a73cc54d7406503d29ebfb75c20c703fae3cfe
Martina350 <martinacamilaleon@gmail.com>
2024-09-17 19:23:13 -0500

    SE CREAN Y EJECUTAN 5 INSERTS CON TODOS LOS CAMPOS DE LA TABLA PRODUCTOS

--- scripts/srcipt1_PRODUCTOS.sql
+++ scripts/srcipt1_PRODUCTOS.sql
@@ -7,3 +7,19 @@ create table productos(
 	stock int not null,
 	constraint productos_pk primary key (codigo)
 )
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (25,'coca-cola','bebida de 2 litros',3.50,125)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (63,'pepsi','bebida de 1 litro',1.50,110)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (206,'trululu','gomitas acidas',0.90,45)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (85,'leche tony','bebida sabor chocolate',0.50,100)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (19,'kinder huevo','chocolate kinder',2.50,335)
+
+select*from productos
