54822df2cac23ccc9b39dc524070a2814723fd02
Martina350 <martinacamilaleon@gmail.com>
2024-09-17 21:42:52 -0500

    SE CREAN Y EJECUTAN 5 INSERTS CON TODOS LOS CAMPOS DE LA TABLA DE VIDEOJUEGOS

--- scripts/script5_VIDEOJUEGOS.sql
+++ scripts/script5_VIDEOJUEGOS.sql
@@ -6,3 +6,22 @@ create table videojuegos(
 	valoracion int not null,
 	constraint videojuegos_pk primary key (codigo)
 )
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('014','minecraft','videojuego de construcción','100')
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('026','roblox','videojuegos en línea','95')
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('030','fortnite','juego multiplataforma gratuito','90')
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('035','clash royal','videojuego de estrategia','100')
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('048','fall guys','videojuego de plataformas y battle royale','85')
+
+select*from videojuegos
+
+
