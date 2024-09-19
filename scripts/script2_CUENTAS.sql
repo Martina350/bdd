bfbc7438b2da9c5e9a8a50583f5d7b93df53186a
Martina350 <martinacamilaleon@gmail.com>
2024-09-18 21:32:37 -0500

    SE ELIMINA TODAS LAS CUENTAS CUYA CEDULA EMPIECE CON 14 (POR QUE NO TENIA CON 10 EN MI REGISTRO)

--- scripts/script2_CUENTAS.sql
+++ scripts/script2_CUENTAS.sql
@@ -60,3 +60,8 @@ where cedula_propietario like '17%'
 
 update cuentas set saldo=10
 where cedula_propietario like '17%'
+
+delete from cuentas
+where cedula_propietario like '14%'
+
+select*from cuentas
