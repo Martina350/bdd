74393d08f84c558d7db367fefacdcfd682383716
Martina350 <martinacamilaleon@gmail.com>
2024-10-22 12:32:02 -0500

    SCRIPTS DE EVALUACION MODULO 2

--- scripts/script_EVALUACION MODULO2.sql
+++ scripts/script_EVALUACION MODULO2.sql
@@ -3,25 +3,32 @@ drop table if exists productos
 drop table if exists categorias
 
 create table categorias (
-    id char(4) primary key,
-    nombre varchar(50) not null
+    id char(4) not null,
+    nombre varchar(50) not null,
+	constraint idcategoria_pk primary key (id)
 )
 
 create table productos (
-    id char(5) primary key,
+    id char(5) not null,
     nombre varchar(100) not null,
     precio_venta money not null,
     precio_compra money not null,
     id_categoria char(4) not null,
-    foreign key (id_categoria) references categorias(id)
+    constraint categorias_fk
+	foreign key (id_categoria) 
+	references categorias(id),
+	constraint id_pk primary key (id)
 )
 
 create table historial_movimientos (
-    id serial primary key,
+    id serial not null,
     id_producto char(5) not null,
     cantidad int not null,
     fecha_movimiento timestamp not null,
-    foreign key (id_producto) references productos(id)
+    constraint productos_fk
+	foreign key (id_producto) 
+	references productos(id),
+	constraint productos_historial_pk primary key (id)
 )
 
 insert into categorias (id, nombre)
@@ -57,10 +64,6 @@ values ('p002', 8, '2020-10-28 10:20');
 insert into historial_movimientos (id_producto, cantidad, fecha_movimiento)
 values ('p001', 12, '2020-10-28 12:45');
 
-
 select * from productos
 select * from categorias
 select * from historial_movimientos
-
-
-
