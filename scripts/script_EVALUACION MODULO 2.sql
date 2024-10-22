26716e1cfa2e5b296a6e1a1931370474d4f41e92
Martina350 <martinacamilaleon@gmail.com>
2024-10-22 11:22:24 -0500

    SCRIPTS DE EVALUACION MODULO 2

---
+++ scripts/script_EVALUACION MODULO 2.sql
@@ -0,0 +1,66 @@
+drop table if exists historial_movimientos
+drop table if exists productos
+drop table if exists categorias
+
+create table categorias (
+    id char(4) primary key,
+    nombre varchar(50) not null
+)
+
+create table productos (
+    id char(5) primary key,
+    nombre varchar(100) not null,
+    precio_venta money not null,
+    precio_compra money not null,
+    id_categoria char(4) not null,
+    foreign key (id_categoria) references categorias(id)
+)
+
+create table historial_movimientos (
+    id serial primary key,
+    id_producto char(5) not null,
+    cantidad int not null,
+    fecha_movimiento timestamp not null,
+    foreign key (id_producto) references productos(id)
+)
+
+insert into categorias (id, nombre)
+values ('c001', 'bebidas');
+
+insert into categorias (id, nombre)
+values ('c002', 'snacks');
+
+insert into categorias (id, nombre)
+values ('c003', 'golosinas');
+
+insert into productos (id, nombre, precio_venta, precio_compra, id_categoria)
+values ('p001', 'coca cola 350 ml', 0.7, 0.55, 'c001');
+
+insert into productos (id, nombre, precio_venta, precio_compra, id_categoria)
+values ('p002', 'coca cola 100 ml', 0.4, 0.25, 'c001');
+
+insert into productos (id, nombre, precio_venta, precio_compra, id_categoria)
+values ('p003', 'doritos 50g', 0.5, 0.35, 'c002');
+
+insert into productos (id, nombre, precio_venta, precio_compra, id_categoria)
+values ('p004', 'manicho', 0.25, 0.15, 'c003');
+
+insert into productos (id, nombre, precio_venta, precio_compra, id_categoria)
+values ('p005', 'tango', 0.5, 0.2, 'c003');
+
+insert into historial_movimientos (id_producto, cantidad, fecha_movimiento)
+values ('p001', 10, '2020-10-28 09:45');
+
+insert into historial_movimientos (id_producto, cantidad, fecha_movimiento)
+values ('p002', 8, '2020-10-28 10:20');
+
+insert into historial_movimientos (id_producto, cantidad, fecha_movimiento)
+values ('p001', 12, '2020-10-28 12:45');
+
+
+select * from productos
+select * from categorias
+select * from historial_movimientos
+
+
+
